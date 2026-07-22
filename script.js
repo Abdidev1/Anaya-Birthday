28e3f7da7f56b94c7b0f24e65b9e6b7d87d65b41
Abdullah Mohsin <abdullahmohsin077@gmail.com>
2026-07-23 02:09:04 +0500

    js file completed update

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ var unlockBtn = document.getElementById("unlock-btn");
 var errorMsg = document.getElementById("error-msg");
 
 function checkPasscode() {
-    if (passInput.value === "2407" {
+    if (passInput.value === "2407") {
         lockScreen.style.display = "none";
     } else {
         errorMsg.innerHTML = "Wrong Code! Try Again.";
         passInput.value = "";
     }
 }
+
+unlockBtn.onclick = checkPasscode;
+
+passInput.onkeypress = function(e) {
+    if (e.key === "Enter") {
+        checkPasscode();
+    }
+};
+
 var btn1 = document.getElementById("p1");
 var btn2 = document.getElementById("p2");
 var btn3 = document.getElementById("p3");
@@ -28,6 +37,18 @@ btn3.onclick = function() {
     resultText.innerHTML = "Music Privilege: You get full control over the music next time."
 }
 
+var openLetterBtn = document.getElementById("open-letter-btn");
+var letterPopup = document.getElementById("letter-popup");
+var closeLetterBtn = document.getElementById("close-letter-btn");
+
+openLetterBtn.onclick = function() {
+    letterPopup.style.display = "block";
+};
+
+closeLetterBtn.onclick = function() {
+    letterPopup.style.display = "none";
+};
+
 var surpriseBtn = document.getElementById("surprise-btn");
 var popup = document.getElementById("best-popup");
 var closeBtn = document.getElementById("close-popup-btn");
@@ -38,4 +59,14 @@ surpriseBtn.onclick = function() {
 
 closeBtn.onclick = function() {
     popup.style.display = "none";
-}
+};
+
+window.onclick = function(e) {
+    if (e.target === letterPopup) {
+        letterPopup.style.display = "none";
+    }
+
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+};

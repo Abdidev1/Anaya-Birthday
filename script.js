a872c5c89856cedb2ca7c8a0881d72320a2c9f62
Abdullah Mohsin <abdullahmohsin077@gmail.com>
2026-07-23 17:44:38 +0500

    javascript file updated (added scratch popup!

--- script.js
+++ script.js
@@ -50,15 +50,15 @@ closeLetterBtn.onclick = function() {
 };
 
 var surpriseBtn = document.getElementById("surprise-btn");
-var popup = document.getElementById("best-popup");
+var bearpopup = document.getElementById("best-popup");
 var closeBtn = document.getElementById("close-popup-btn");
 
 surpriseBtn.onclick = function() {
-    popup.style.display = "flex";
+    bearpopup.style.display = "flex";
 };
 
 closeBtn.onclick = function() {
-    popup.style.display = "none";
+    bearpopup.style.display = "none";
 };
 
 window.onclick = function(e) {
@@ -67,6 +67,75 @@ window.onclick = function(e) {
     }
 
     if (e.target === popup) {
-        popup.style.display = "none";
+        bearpopup.style.display = "none";
+    }
+};
+
+var openScratchBtn = document.getElementById("open-scratch-btn");
+var scratchPopup = document.getElementById("scratch-popup");
+var closeScratchBtn = document.getElementById("close-scratch-btn");
+var canvas = document.getElementById("scratch-canvas");
+var ctx = document.getElementById("2d");
+var giftCodeText = document.getElementById("gift-code-text");
+
+var realRobloxCode = "9154-9264-9245-4865";
+
+giftCodeText.innerHTML = realRobloxCode;
+
+var isScratch = false;
+
+function initScratchCanvas() {
+    ctx.globalCompositeOperation = "source-over";
+    ctz.fillStyle = "#888888";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#ffffff";
+    ctx.font = "bold 16px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText = ("SCRATCH HERE!", camnvas.width / 2, canvas.height / 2 + 6);
+}
+
+openScratchBtn.onclick = function() {
+    scratchPopup.style.display = "block";
+    initScratchCanvas();
+};
+
+closeScratchBtn.onclick = function() {
+    scratchPopup.style.display = "none";
+};
+
+function scratch(e) {
+    if (!isScratching) return;
+
+    var rect = canvas.getBoundingClientRect();
+    var clientX = e.ClientX || (e.touches && e.touches[0].ClientX);
+    var clientY = e.clientY || (e.touches && e.touches[0].clientY);
+
+    var x = clientX - rect.left;
+    var y = clientY - rect.top;
+
+    ctx.globalCompositeOperation = "destination-out";
+    ctx.beginPath();
+    ctx.arc(x,y,18,0, Math.PI * 2);
+    ctx.fill;
+}
+
+canvas.addEventListener("mousedown", function(e) { isScratching = true; scratch(e); });
+canvas.addEventListener("mousemove", scratch);
+window.addEventListener("mouseup", function() { isScratching = false; });
+
+canvas.addEventListener("touchstart", function(e) { isScratching = true; scratch(e); });
+canvas.addEventListener("touchmove", scratch);
+window.addEventListener("touchend", function() { isScratching = false; });
+
+window.onclick = function(e) {
+    if (e.target === letterPopup) {
+        letterPopup.style.display = "none";
+    }
+    if (e.target === bearpopup) {
+        bearpopup.style.display = "none";
+    }
+    if (e.target === scratchPopup) {
+        scratchPopup.style.display = "none";
     }
 };
